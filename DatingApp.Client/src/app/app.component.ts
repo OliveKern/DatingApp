56b3cc6db80585eede0bc427bc7c3b0d323f0316
OliverKern <odk@gmx.at>
2024-11-13 17:36:19 +0100

    added register form to client

--- DatingApp.Client/src/app/app.component.ts
+++ DatingApp.Client/src/app/app.component.ts
@@ -1,24 +1,20 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { NavComponent } from "./nav/nav.component";
 import { RouterOutlet } from '@angular/router';
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
-  title = 'DatingApp'
   private accountService = inject(AccountService);
-  http = inject(HttpClient);
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -29,13 +25,5 @@ export class AppComponent implements OnInit{
 
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
   }
 }
---
+++ DatingApp.Client/src/app/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  // @Input({required: true}) usersFromHomeComponent: any;
+  usersFromHomeComponent = input.required<any>();
+  model: any = {}
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+
+}
