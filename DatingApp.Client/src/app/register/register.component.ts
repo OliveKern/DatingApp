c0a31858e68609fadb3049ea27dae183e6db6220
OliveKern <odk@gmx.at>
2024-11-17 16:46:50 +0100

    finished client login and register

--- DatingApp.Client/src/app/register/register.component.ts
+++ DatingApp.Client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, inject, input, output, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -9,16 +10,29 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  private accountService = inject(AccountService);
+
   // @Input({required: true}) usersFromHomeComponent: any;
-  usersFromHomeComponent = input.required<any>();
+  //usersFromHomeComponent = input.required<any>();
+  
+  //@Output() cancelRegister = new EventEmitter();
+  cancelRegister = output<boolean>();
+
   model: any = {}
 
+  
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error)
+    })
   }
 
   cancel() {
-    console.log('cancelled');
+    this.cancelRegister.emit(false);
   }
 
 }
