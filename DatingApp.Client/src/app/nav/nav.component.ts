f6ef392084e048fffbd6ad75510fea9a9f6d3f17
OliveKern <odk@gmx.at>
2024-12-08 18:11:43 +0100

    Added a dummy route and a new theme

--- DatingApp.Client/src/app/nav/nav.component.ts
+++ DatingApp.Client/src/app/nav/nav.component.ts
@@ -4,11 +4,12 @@ import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink ,FormsModule, BsDropdownModule, RouterLinkActive],
+  imports: [RouterLink ,FormsModule, BsDropdownModule, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
